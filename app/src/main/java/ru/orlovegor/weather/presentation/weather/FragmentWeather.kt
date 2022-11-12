da208cee328c8715e73b0287551317b4862ddd87
Orlov Egor <dbn.orlov@gmail.com>
2022-11-12 23:30:03 +0300

    add logic load and set data to Ui

--- app/src/main/java/ru/orlovegor/weather/presentation/weather/FragmentWeather.kt
+++ app/src/main/java/ru/orlovegor/weather/presentation/weather/FragmentWeather.kt
@@ -1,27 +1,63 @@
 package ru.orlovegor.weather.presentation.weather
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.navArgs
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import ru.orlovegor.weather.R
 import ru.orlovegor.weather.databinding.FragmentWeatherBinding
-import ru.orlovegor.weather.utils.Cities
+import ru.orlovegor.weather.utils.ItemOffsetDecoration
+import ru.orlovegor.weather.utils.makeToast
 
 @AndroidEntryPoint
 class FragmentWeather : Fragment(R.layout.fragment_weather) {
 
     private val binding: FragmentWeatherBinding by viewBinding()
     private val viewModel: WeatherViewModel by viewModels()
-    private val city: FragmentWeatherArgs by navArgs()
+
+    private val weatherAdapter by lazy(LazyThreadSafetyMode.NONE) {
+        WeatherAdapter()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Toast.makeText(requireContext(), city.city, Toast.LENGTH_SHORT).show()
+        initList()
+        observeViewModelState()
+    }
 
+    private fun observeViewModelState() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.weather.collectLatest { weatherPerHour ->
+                weatherAdapter.submitList(weatherPerHour)
+            }
+        }
+        lifecycleScope.launchWhenStarted {
+            viewModel.isProgress.collectLatest { isProgress ->
+                binding.weatherProgress.isVisible = isProgress
+            }
+        }
+        lifecycleScope.launchWhenStarted {
+            viewModel.toast.collectLatest { messageRes ->
+                makeToast(requireContext(), messageRes)
+            }
+        }
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun initList() {
+        with(binding.listWeather) {
+            adapter = weatherAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            addItemDecoration(ItemOffsetDecoration(requireContext()))
+        }
     }
 }
--- app/src/main/java/ru/orlovegor/weather/presentation/weather/WeatherViewModel.kt
+++ app/src/main/java/ru/orlovegor/weather/presentation/weather/WeatherViewModel.kt
@@ -1,22 +1,59 @@
 package ru.orlovegor.weather.presentation.weather
 
-import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import ru.orlovegor.weather.R
 import ru.orlovegor.weather.data.repositorises.WeatherRepository
+import ru.orlovegor.weather.presentation.models.WeatherPerHour
+import ru.orlovegor.weather.utils.ResultWrapper
 import javax.inject.Inject
 
 @HiltViewModel
-class WeatherViewModel @Inject constructor (
-private val weatherRepository: WeatherRepository
-): ViewModel() {
+class WeatherViewModel @Inject constructor(
+        private val stateNavArgs: SavedStateHandle,
+        private val weatherRepository: WeatherRepository
+) : ViewModel() {
+
+        private val _city = MutableSharedFlow<String>()
+        private val _weather = MutableStateFlow(listOf<WeatherPerHour>())
+        private val _isProgress = MutableStateFlow(false)
+        private val _toast = MutableSharedFlow<Int>()
+        val weather = _weather.asStateFlow()
+        val isProgress = _isProgress.asStateFlow()
+        val toast = _toast.asSharedFlow()
 
         init {
-                Log.d("DATA", "WM - INIT")
+                load()
                 viewModelScope.launch {
-                        weatherRepository.fetchWeatherByCity("Moscow")
+                        stateNavArgs.get<String>(NAV_ARG_KEY_CITY)?.let { _city.emit(it) }
                 }
+        }
+
+        private fun load() {
+                _city.onEach {
+                        _isProgress.value = true
+                                when (val data = weatherRepository.fetchWeatherByCity(it)) {
+                                        is ResultWrapper.Success -> {
+                                                _weather.emit(data.value)
+                                        }
+                                        is ResultWrapper.NetworkError -> {
+                                                _toast.emit(R.string.network_error)
+                                        }
+                                        is ResultWrapper.Error -> {
+                                                _toast.emit(R.string.error)
+                                        }
+                                }
+                        }
+                        .onEach { _isProgress.value = false }
+                        .launchIn(viewModelScope)
+
+        }
+
+        companion object {
+                const val NAV_ARG_KEY_CITY = "city"
         }
 }
