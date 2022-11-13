a8e6c011251af99f0e9be0aa354357ec841e9e4e
Orlov Egor <dbn.orlov@gmail.com>
2022-11-14 01:45:36 +0300

    added method and this realization(request database CRUD)

--- app/src/main/java/ru/orlovegor/weather/data/repositorises/WeatherRepository.kt
+++ app/src/main/java/ru/orlovegor/weather/data/repositorises/WeatherRepository.kt
@@ -1,9 +1,14 @@
 package ru.orlovegor.weather.data.repositorises
 
+import ru.orlovegor.weather.data.local.entity.LocalCity
 import ru.orlovegor.weather.presentation.models.WeatherPerHour
 import ru.orlovegor.weather.utils.ResultWrapper
 
 interface WeatherRepository {
 
-    suspend fun fetchWeatherByCity(city: String): ResultWrapper<List<WeatherPerHour>>
+   suspend fun fetchWeatherByCity(city: String): ResultWrapper<List<WeatherPerHour>>
+
+   suspend fun saveDataStrategy(city: LocalCity, weathersPerHour: List<WeatherPerHour>)
+
+   suspend fun loadDataStrategy(cityId: Long, currentDate: String): List<WeatherPerHour>
 }
